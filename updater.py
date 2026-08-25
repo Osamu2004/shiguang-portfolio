921d2783d90bc4cf929b01ebe109e59fef59f219
Osamu2004 <1153510039@qq.com>
2026-08-26 05:59:11 +0800

    Show gaps for incomplete fund history

--- updater.py
+++ updater.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 import certifi
 
-VERSION = "0.20.7"
+VERSION = "0.20.8"
 REPO = "Osamu2004/shiguang-portfolio"
 SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
 
