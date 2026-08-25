89decdd14e06c82ddb18ae9e906a18be5ada2f96
Osamu2004 <1153510039@qq.com>
2026-08-25 23:58:06 +0800

    Simplify NGC coin entry form

--- updater.py
+++ updater.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 import certifi
 
-VERSION = "0.17.1"
+VERSION = "0.17.2"
 REPO = "Osamu2004/shiguang-portfolio"
 SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
 
