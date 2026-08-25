9c871de7996f79705616729af74428531ca8eccd
Osamu2004 <1153510039@qq.com>
2026-08-26 06:18:00 +0800

    Add value labels to asset trend axis

--- updater.py
+++ updater.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 import certifi
 
-VERSION = "0.21.1"
+VERSION = "0.21.2"
 REPO = "Osamu2004/shiguang-portfolio"
 SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
 
