73da36f3a737bc35c281f2c6f701d9b6b2c78a11
Osamu2004 <1153510039@qq.com>
2026-08-26 06:32:44 +0800

    Redesign drawdown strategy editor as modal

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,3 @@
-const CACHE='shiguang-v30';
+const CACHE='shiguang-v31';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/style.css','/dashboard.css','/checkup.css','/accounts.css','/modules.css','/app.js']))));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&!e.request.url.includes('/api/'))e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
